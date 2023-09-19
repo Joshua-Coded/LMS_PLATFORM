5986d23e353453cb03571397ad89f953223f9aa9
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-19 11:19:02 +0200

    testing

--- lms/app/page.tsx
+++ lms/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
+    <div>
       <p className='text-3xl font-medium text-sky-700'>hi there</p>    
+      <Button variant="destructive">
+        click me
+      </Button>
+      
+      </div>
   )
 }
