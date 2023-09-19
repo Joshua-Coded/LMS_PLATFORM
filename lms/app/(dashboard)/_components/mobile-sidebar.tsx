87b068f6b83f99db7326976b6a6c22943c99fb9c
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-19 23:28:02 +0200

    sidebar-item

---
+++ lms/app/(dashboard)/_components/mobile-sidebar.tsx
@@ -0,0 +1,8 @@
+import { Menu } from "lucide-react"
+
+
+export const MobileSidebar = () => {
+    return (
+        <Menu />
+    )
+}
---
+++ lms/app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,11 @@
+import { MobileSidebar } from "./mobile-sidebar"
+
+export const Navbar = () => {
+    return (
+        <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+            {/* mobile */}
+
+            <MobileSidebar />
+        </div>
+    )
+}
