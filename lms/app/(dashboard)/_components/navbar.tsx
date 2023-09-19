b22b84b0197b10ce35b1b75617cb0850ea7711e6
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-19 23:47:10 +0200

    sidebar-item

--- lms/app/(dashboard)/_components/navbar.tsx
+++ lms/app/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,12 @@
+import { NavbarRoutes } from "@/components/navbar-routes"
 import { MobileSidebar } from "./mobile-sidebar"
 
 export const Navbar = () => {
     return (
         <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
             {/* mobile */}
-
             <MobileSidebar />
+            <NavbarRoutes />
         </div>
     )
 }
