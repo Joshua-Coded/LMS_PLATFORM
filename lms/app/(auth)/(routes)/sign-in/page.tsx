11941cfbf2db981f2d8cfc971c68068365da51cd
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-09-19 19:09:20 +0200

    rounting

---
+++ lms/app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+const SignInPage = () => {
+    return ( 
+        <div>
+            <p>
+            this is an unprotected page!!
+            </p>
+        </div>
+     );
+}
+ 
+export default SignInPage;
